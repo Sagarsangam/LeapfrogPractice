c2e4efa7336def6b1f68558c7ecc122362a74c76
Sagarsangam <sagarsangamrai@gmail.com>
2016-06-22 18:19:17 +0545

    First commit for Loop added

--- FirstJava/src/com/sagar/firstjava/Program.java
+++ FirstJava/src/com/sagar/firstjava/Program.java
@@ -15,7 +15,8 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+for(int i=1;i<=9;i++){
+    System.out.println(i);
+}}
     
 }
